0ea0411fcec5d9b17b4569c0f356617ff19a8c13
Hyeri Yang <82886104+HyeriYangs@users.noreply.github.com>
2021-05-22 14:37:39 +0900

    Add files via upload
    결과 : 6 
    500-2, 100-2, 50-1, 10-1 이므로 총 동전의 개수 n = 6
    
    화폐의 종류만큼 반복수행하므로, 화폐의 종류가 K개라고 하면 이 코드의 시간 복잡도는 O(K)이다. 
    => 이 알고리즘의 시간 복잡도는 동전의 총 종류에만 영향을 받고, 거슬러 줘야 할 금액의 크기와는 무관하다.

---
+++ 거스름돈.py
@@ -0,0 +1,12 @@
+n = 1260 # 거스름돈
+count = 0 
+
+# 거스름돈 단위 : 500원, 100원, 50원, 10원
+coin_types = [500, 100, 50, 10 ]
+
+for coin in coin_types:
+  count += n // coin  #해당 화폐로 거슬러 줄 동전의 개수 세기 
+  n %= coin 
+
+print(count)
+
